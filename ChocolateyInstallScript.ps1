0cee8d4f53fb505ec72535ba53ace1e991a30a9b
lieven-VanDenBerghe <70700796+lieven-VanDenBerghe@users.noreply.github.com>
2022-01-02 16:31:04 +0100

    upload ChocolateyInstallScript.ps1

---
+++ ChocolateyInstallScript.ps1
@@ -0,0 +1,22 @@
+﻿#install chocolatey CLI
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+choco upgrade chocolatey
+
+#install chocolatey GUI
+choco install -y chocolateygui
+choco upgrade chocolateygui
+
+#choco list .... #to find a package to install
+
+#programs to install
+choco install -y firefox
+choco upgrade firefox
+
+choco install -y github-desktop
+choco upgrade github-desktop
+
+choco install -y 7zip
+choco upgrade 7zip
+
+choco install -y vscode
+choco upgrade vscode
