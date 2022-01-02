d28f9be48e146a467bc56e6a87fb4f2a15d59a0f
lieven-VanDenBerghe <70700796+lieven-VanDenBerghe@users.noreply.github.com>
2022-01-02 18:51:36 +0100

    update ChocolateyInstallScript.ps1

--- ChocolateyInstallScript.ps1
+++ ChocolateyInstallScript.ps1
@@ -1,6 +1,7 @@
 ﻿#install chocolatey CLI
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco upgrade chocolatey
+choco feature enable -n allowGlobalConfirmation
 
 #install chocolatey GUI
 choco install -y chocolateygui
@@ -24,5 +25,53 @@ choco upgrade vscode
 choco install -y vscode
 choco upgrade vscode
 
-choco install -y pandoc  # voor markdown in vscode
-choco upgrade pandoc
+choco install -y libreoffice-fresh
+choco upgrade libreoffice-fresh
+
+choco install -y google-drive-file-stream
+choco upgrade google-drive-file-stream
+
+choco install -y spotify
+choco upgrade spotify
+
+choco install -y ccleaner
+choco upgrade ccleaner
+
+choco install -y filezilla
+choco upgrade filezilla
+
+choco install -y virtualbox
+choco upgrade virtualbox
+
+choco install -y virtualbox-guest-additions-guest.install
+choco upgrade virtualbox-guest-additions-guest.install
+
+choco install -y steam-client
+choco upgrade steam-client
+
+choco install -y epicgameslauncher
+choco upgrade epicgameslauncher
+
+choco install -y discord
+choco upgrade discord
+
+choco install -y ps-remote-play
+choco upgrade ps-remote-play
+
+choco install -y minecraft-launcher
+choco upgrade minecraft-launcher
+
+choco install -y Runescape
+choco upgrade Runescape
+
+choco install -y runelite
+choco upgrade runelite
+
+choco install -y osu
+choco upgrade osu
+
+choco install -y bitwarden
+choco upgrade bitwarden
+
+choco install -y leagueoflegends
+choco upgrade leagueoflegends
